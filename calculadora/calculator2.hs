255a5d66e73fb7bccfab3cbe934177948fc452f1
Jose Jimenez <jljb1990@gmail.com>
2015-02-16 21:36:13 -0430

    Agregado el nuevo lexer

--- calculadora/calculator2.hs
+++ calculadora/calculator2.hs
@@ -4,39 +4,82 @@ import Text.Parsec.Error
 import Text.ParserCombinators.Parsec
 import qualified Control.Applicative as AP
  
+data Token =   TokPlus 
+             | TokMinus 
+             | TokStar 
+             | TokSlash 
+             | TokEnd 
+             | TokComma 
+             | TokLeftParent 
+             | TokRightParent 
+             | TokInteger String
+             | TokError
+      deriving (Show, Read)
+
+lexer :: Parser (Token)
+lexer = spaces >> (      (char '+' >> spaces >> return TokPlus)
+                     <|> (char '-' >> spaces >> return TokMinus)
+                     <|> (char '*' >> spaces >> return TokStar)
+                     <|> (char '/' >> spaces >> return TokSlash)
+                     <|> (char ',' >> spaces >> return TokComma)
+                     <|> (char '(' >> spaces >> return TokLeftParent)
+                     <|> (char ')' >> spaces >> return TokRightParent)
+                     <|> (char '$' >> spaces >> return TokEnd)
+                     <|> ((many1 digit) AP.<* spaces >>= return . (TokInteger))
+                     <|> (anyChar >> return TokError))
+
 expr :: Parser (Either [ParseError] Integer)
 expr = do t <- term
-          do try (cleanSpaces(char '+')) >> expr >>= return . (verifyBinError (+) t)
-             <|> (try (cleanSpaces(char '-')) >> expr >>= return . (verifyBinError (-) t))
-             <|> (lookAhead(char '$' <|> char ',') >> return t)
-             <|> (genNewError panicMode "operador" >>= return . (checkError t))
+          do (lookAhead(char '$' <|> char ',' <|> char ')') >> return t)
+              <|> do tok <- lexer
+                     case tok of
+                       { TokPlus    -> expr >>= return . (verifyBinError (+) t)
+                       ; TokMinus   -> expr >>= return . (verifyBinError (-) t)
+                       ; TokError   -> (genNewError (panicMode lookAheadSet) "operador" tok >>= return . (checkError t))
+                       ; otherwise  -> (genNewError (panicMode lookAheadSetList) "fin de archivo o coma" tok >>= return . (checkError t))
+                       }
 
 term = do p <- factor
-          do try (cleanSpaces(char '*')) >> term >>= return . (verifyBinError (*) p)
-             <|> (try (cleanSpaces(char '/')) >> term >>=  return . (verifyBinError (div) p))
-             <|> (lookAhead(char '+' <|> char '-' <|> char '$' <|> char ',') >> return p)
-             <|> (genNewError panicMode "operador" >>= return . (checkError p))
+          do (lookAhead(char '+' <|> char '-' <|> char '$' <|> char ')' <|> char ',') >> return p)
+              <|> do tok <- lexer
+                     case tok of
+                       { TokSlash   -> term >>= return . (verifyBinError (div) p)
+                       ; TokStar    -> term >>= return . (verifyBinError (*)   p)
+                       ; TokError   -> (genNewError (panicMode lookAheadSet) "operador" tok >>= return . (checkError p))
+                       ; otherwise  -> (genNewError (panicMode lookAheadSetList) "fin de archivo, operador o coma" tok >>= return . (checkError p))
+                       }
 
-factor = do try(cleanSpaces(char '('))
-            e <- expr
-            do try(cleanSpaces(char ')')) >> return e
-               <|> (genNewError panicMode ")" >>= return . Left . return)
-      <|> number
-  
-number = do ds <- cleanSpaces(many1 digit)
-            return (Right (read ds))
-      <|> (genNewError panicMode "numero" >>= return . Left . return)
-
-genNewError pm msg = do pos  <- getPosition
-                        ys   <- getInput
-                        pm
-                        return (newErrorMessage (Message ("Esperaba un " ++ msg ++ " en vez de " ++ [head ys])) pos)
+factor = do tok <- lexer
+            case tok of
+              { TokLeftParent  -> do e     <- expr 
+                                     tok'  <- lexer
+                                     case tok' of
+                                       { TokRightParent -> return e                                     
+                                       ; TokError       ->  (genNewError (panicMode lookAheadSet) ")" tok' >>= return . Left . return)
+                                       }
+              ; TokInteger int -> return (Right (read int))
+              ; TokError       -> (genNewError (panicMode lookAheadSet) "numero" tok >>= return . Left . return)
+              }
+     
+genNewError pm msg e = do pos  <- getPosition
+                          pm
+                          return (newErrorMessage (Message ("Esperaba un " ++ msg ++ " en vez de " ++ (show e))) pos)
 
 parseListExpr :: Parser (Either [ParseError] [Integer])
 parseListExpr = do  spaces
                     e <- expr
-                    do try (cleanSpaces(char ',')) >> parseListExpr >>= return . (verifyBinError (:) e)
-                       <|> (try(char '$') >> return (fmap (AP.pure) e))
+                    do tok <- lexer
+                       case tok of
+                        { TokComma  -> parseListExpr >>= return . (verifyBinError (:) e)
+                        ; TokEnd    -> return (fmap (AP.pure) e)
+                        ; otherwise -> do err <- genNewError (panicMode lookAheadSetList) "fin de archivo, operador o coma" tok
+                                          tok' <- lexer
+                                          case tok' of
+                                          { TokComma -> do res <- parseListExpr
+                                                           return (verifyBinError (:) (checkError e err) res)
+                                          ; TokEnd   -> return (checkError e err)
+                                          }
+                        }
 
 checkError (Left xs) s = Left (s:xs)
 checkError _         s = Left [s] 
@@ -46,15 +89,13 @@ verifyBinError _  (Left xs) _         = Left xs
 verifyBinError _  _         (Left ys) = Left ys
 verifyBinError op (Right x) (Right y) = Right(op x y) 
 
-panicMode = manyTill anyChar (lookAhead(char '-' <|> char '*' <|> char '/' <|> char '+' <|> char '$' <|> char ','))
+lookAheadSet = lookAhead(char '-' <|> char '*' <|> char '/' <|> char '+' <|> char '$' <|> char ',' <|> char '(' <|> char ')')
 
+lookAheadSetList = lookAhead(char ',' <|> char '$')
 
-cleanSpaces p = do  r <- p
-                    spaces
-                    return r
+panicMode until = manyTill anyChar until 
 
-play :: String -> IO ()
-play inp = case runParser parseListExpr () "" (inp ++ "$") of
-             { Left err -> print err
-             ; Right ans -> print ans
-             }
+play parser inp = case runParser (parser) () "" (inp ++ "$") of
+                    { Left err -> print err
+                    ; Right ans -> print ans
+                    }
