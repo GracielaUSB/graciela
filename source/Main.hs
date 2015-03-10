467136bf17594066a54e7ea45bd55325f0ff3a36
Jose Jimenez <jljb1990@gmail.com>
2015-03-10 16:05:01 -0430

    El lexer del lenguaje imprime bonito y agregado un caso de ejemplo

--- source/Main.hs
+++ source/Main.hs
@@ -4,26 +4,27 @@ import Text.Parsec
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import Control.Applicative as AP
-import Parser
+--import Parser
 import Lexer
 import Token
 
 playLexer inp = case runParser (lexer) () "" (inp) of
-            { Left  err -> putStrLn $ "Ocurrio un error en el proceso de parseo " ++ (show err)
-            ; Right par -> putStrLn (show par)
-            }
+                { Left  err     -> putStrLn $ "Ocurrio un error en el proceso de parseo " ++ (show err)
+                ; Right par'''  -> putStr $ concatMap (\(t,sp) -> (showSP sp) ++ (show t) ++ "\n") par'''
+                }
 
-playParser inp = runParser (parseExpr) () "" (inp)
+showSP sp = "Línea: " ++ show (sourceLine sp) ++ ", Columna: " ++ show (sourceColumn sp) ++ " "
+-- playParser inp = runParser (parseExpr) () "" (inp)
 
-concatLexPar = playParser AP.<$> lexer
+--concatLexPar = playParser AP.<$> lexer
 
-play inp = case runParser (concatLexPar) () "" (inp) of
-            { Left  err -> putStrLn $ "Ocurrio un error en el proceso de parseo " ++ (show err)
-            ; Right par -> case par of
-                             { Left  err' -> putStrLn $ "Ocurrio un error lexicografico " ++ (show err')
-                             ; Right par' -> putStrLn $ show par'
-                             }
-            }
+--play inp = case runParser (concatLexPar) () "" (inp) of
+--            { Left  err -> putStrLn $ "Ocurrio un error en el proceso de parseo " ++ (show err)
+--            ; Right par -> case par of
+--                             { Left  err' -> putStrLn $ "Ocurrio un error lexicografico " ++ (show err')
+--                             ; Right par' -> putStrLn $ show par'
+--                             }
+--            }
                            
 parseFromFile :: FilePath -> IO ()
 parseFromFile arch = do  s <- TIO.readFile arch
