804a604f316a6d2cab8e01e3046a54ee7b4833df
Jose Jimenez <jljb1990@gmail.com>
2015-07-18 22:55:54 -0430

    Primera version de la verificacion de tipos. Electricista

--- source/Codegen.hs
+++ source/Codegen.hs
@@ -13,15 +13,42 @@ import IR
 import qualified Type as T
 import Data.Word
 import qualified LLVM.General.AST.Constant as C
+import LLVM.General.Module
+import qualified LLVM.General.AST as AST
+import qualified Data.Text as T
+import Data.Foldable (toList)
 
 data CodegenSt
   = CodeGenSt {
     count   :: Word              -- Cantidad de instrucciones sin nombre
-  , instrs  :: DS.Seq Instruction  -- Lista de instrucciones del programa
+  , instrs  :: DS.Seq (Named Instruction)  -- Lista de instrucciones del programa
   } deriving (Show)
 
 newtype Codegen a = Codegen { runCodegen :: State CodegenSt a }
   deriving (Functor, Applicative, Monad, MonadState CodegenSt)
+
+newtype LLVM a = LLVM { unLLVM :: State AST.Module a }
+  deriving (Functor, Applicative, Monad, MonadState AST.Module)
+
+runLLVM :: AST.Module -> LLVM a -> AST.Module
+runLLVM = flip (execState . unLLVM)
+
+emptyModule :: String -> AST.Module
+emptyModule label = defaultModule { moduleName = label }
+
+addDefn :: Definition -> LLVM ()
+addDefn d = do
+  defs <- gets moduleDefinitions
+  modify $ \s -> s { moduleDefinitions = defs ++ [d] }
+
+defineProc :: String -> [(Type, Name)] -> [BasicBlock] -> LLVM ()
+defineProc label argtys body = addDefn $
+  GlobalDefinition $ functionDefaults {
+    name        = Name label
+  , parameters  = ([Parameter ty nm [] | (ty, nm) <- argtys], False)
+  , returnType  = VoidType
+  , basicBlocks = body
+  }
 
 emptyCodegen :: CodegenSt
 emptyCodegen = CodeGenSt 0 DS.empty
@@ -42,9 +69,18 @@ instr :: Type -> Instruction -> Codegen (Operand)
 instr t ins = do
   xs <- gets instrs
   n  <- getCount
-  modify $ \s -> s { instrs = xs DS.|> ins }
-  return $ local t (UnName n)
+  let ref = UnName n
+  modify $ \s -> s { instrs = xs DS.|> (ref := ins) }
+  return $ local t ref
 
+astToLLVM :: MyAST.AST T.Type -> LLVM ()
+astToLLVM (MyAST.Program name _ _ (acc:_) _) = do
+  defineProc (T.unpack name) [] bls
+  where
+    bls = createBlocks $ execCodegen $ astToInstr acc
+
+createBlocks :: CodegenSt -> [BasicBlock]
+createBlocks st = [BasicBlock (Name "program") (toList (instrs st)) ((Name "final") := (Ret Nothing []))]
   
 astToInstr :: MyAST.AST T.Type -> Codegen (Operand)
 astToInstr (MyAST.Arithmetic op _ lexp rexp t) = do
@@ -54,6 +90,25 @@ astToInstr (MyAST.Arithmetic op _ lexp rexp t) = do
 
 astToInstr (MyAST.Int _ n _) = do
   return $ ConstantOperand $ C.Int 32 n
+
+astToInstr (MyAST.LAssign (((id, t), _):_) (e:_) _ _) = do
+  e' <- astToInstr e
+  let (t', r) = (toType t, (Name (T.unpack id)))
+  i <- alloca t' r
+  store t' i e'
+  return i
+
+astToInstr (MyAST.Block _ _ (a:_) _) = do
+  astToInstr a
+
+alloca :: Type -> Name -> Codegen Operand
+alloca ty r = do
+  xs <- gets instrs
+  modify $ \s -> s { instrs = xs DS.|> (r := (Alloca ty Nothing 0 [])) }
+  return $ local ty r
+
+store :: Type -> Operand -> Operand -> Codegen Operand
+store t ptr val = instr t $ Store False ptr val Nothing 0 []
 
 toType :: T.Type -> Type
 toType T.MyInt = IntegerType 32
--- source/Main.hs
+++ source/Main.hs
@@ -19,25 +19,34 @@ import State
 import Lexer
 import AST
 import Codegen
+import LLVM.General.Module
+import LLVM.General.Context
+import Control.Monad.Except
 
 concatLexPar = playParser AP.<$> lexer
-
 
 runStateParse p sn inp init = runIdentity $ ST.runStateT (runPT p () sn inp) init
 
 
-playParser inp = runStateParse (exprLevel7 parseSemicolon parseSemicolon) "" inp initialState
+playParser inp = runStateParse (program) "" inp initialState
 
 
 playLexer inp = putStrLn $ show $ runParser lexer () "" inp
 
+liftError :: ExceptT String IO a -> IO a
+liftError = runExceptT >=> either fail return
 
 play inp = case (runParser (concatLexPar) () "" (inp)) of
             		  { Left  err -> putStrLn $ "Ocurrio un error lexicografico " ++ (show err)
             		  ; Right par -> case par of
            		                     { (Left  err', _ ) -> putStrLn $ "Ocurrio un error en el proceso de parseo " ++ (show err')
-                                   ; (Right (Just ast) , st) -> do putStrLn $ drawState st
-                                                                   putStrLn $ show $ execCodegen $ astToInstr $ fst $ runTVerifier (symbolTable st) ast
+                                   ; (Right (Just ast) , st) -> 
+                                      do let newast = runLLVM (emptyModule "hola bb") $ astToLLVM $ fst $ runTVerifier (symbolTable st) ast
+                                         withContext $ \context ->
+                                            liftError $ withModuleFromAST context newast $ \m -> do
+                                              l <- moduleLLVMAssembly m
+                                              putStrLn l
+                                             
                                                                     
                                    ; (Right  _         , st) -> putStrLn $ drawState st
                                    }
