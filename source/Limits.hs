53c65aa94a62acfde577c0e5f8c50aec7d31ebc6
Jose Jimenez <jljb1990@gmail.com>
2015-09-22 15:56:53 -0430

    Falto el archivo Limits.hs

---
+++ source/Limits.hs
@@ -0,0 +1,13 @@
+module Limits where
+
+maxDouble :: Double
+maxDouble = 10 ** 308.25
+
+maxInteger :: Integer
+maxInteger = 2 ^ 31 - 1
+
+minInteger :: Integer
+minInteger = - (2 ^ 31)
+
+minDouble :: Double
+minDouble  = -1 * maxDouble
