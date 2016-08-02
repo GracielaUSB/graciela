1ee3f6ea4c6aaa43ca44bd38a8796933cba3d86f
Moisés Ackerman <akrmn@users.noreply.github.com>
2016-08-02 13:17:23 -0400

    made parser primitives more generic

--- source/Parser/Token.hs
+++ source/Parser/Token.hs
@@ -7,8 +7,11 @@ Contiene los analizadores semánticos (parsers) básicos del compilador,
 que funcionan como bloques para analizadores semánticos más complejos.
 -}
 
-{-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE TupleSections  #-}
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE NamedFieldPuns    #-}
+{-# LANGUAGE TupleSections     #-}
+{-# LANGUAGE TypeFamilies      #-}
 
 module Parser.Token
   ( anyToken
@@ -27,7 +30,6 @@ module Parser.Token
   , floatLit
   ) where
 --------------------------------------------------------------------------------
-import           Graciela
 import           Token
 --------------------------------------------------------------------------------
 import           Data.List.NonEmpty   (NonEmpty ((:|)))
@@ -35,23 +37,26 @@ import           Data.Set             (Set)
 import qualified Data.Set             as Set
 import           Data.Text            (Text)
 import           Text.Megaparsec      (ErrorItem (Tokens), token, between)
-import           Text.Megaparsec      hiding (Token, satisfy, oneOf)
+import           Text.Megaparsec.Prim (MonadParsec)
+import qualified Text.Megaparsec.Prim as Prim (Token)
 --------------------------------------------------------------------------------
 
 unex :: TokenPos -> (Set (ErrorItem TokenPos), Set a, Set b)
 unex = (, Set.empty, Set.empty) . Set.singleton . Tokens . (:|[])
 
 
-match :: Token -> Graciela Token
+match :: (MonadParsec e s m, Prim.Token s ~ TokenPos)
+      => Token -> m ()
 match t = token test Nothing
   where
     test tp @ TokenPos {tok} =
       if t == tok
-        then Right t
+        then Right ()
         else Left . unex $ tp
 
 
-satisfy :: (Token -> Bool) -> Graciela Token
+satisfy :: (MonadParsec e s m, Prim.Token s ~ TokenPos)
+        => (Token -> Bool) -> m Token
 satisfy f = token test Nothing
   where
     test tp @ TokenPos {tok} =
@@ -60,68 +65,80 @@ satisfy f = token test Nothing
         else Left . unex $ tp
 
 
-oneOf :: [Token] -> Graciela Token
+oneOf :: (MonadParsec e s m, Prim.Token s ~ TokenPos)
+      => [Token] -> m Token
 oneOf ts = satisfy (`elem` ts)
 
 
-parens :: Graciela a -> Graciela a
+parens :: (MonadParsec e s m, Prim.Token s ~ TokenPos)
+       => m a -> m a
 parens = between (match TokLeftPar) (match TokRightPar)
 
 
-percents :: Graciela a -> Graciela a
+percents :: (MonadParsec e s m, Prim.Token s ~ TokenPos)
+         => m a -> m a
 percents = between (match TokLeftPercent) (match TokRightPercent)
 
 
-brackets :: Graciela a -> Graciela a
+brackets :: (MonadParsec e s m, Prim.Token s ~ TokenPos)
+         => m a -> m a
 brackets = between (match TokLeftBracket) (match TokRightBracket)
 
 
-beginEnd :: Graciela a -> Graciela a
+beginEnd :: (MonadParsec e s m, Prim.Token s ~ TokenPos)
+         => m a -> m a
 beginEnd = between (match TokBegin) (match TokEnd)
 
 
-anyToken :: Graciela Token
+anyToken :: (MonadParsec e s m, Prim.Token s ~ TokenPos)
+         => m Token
 anyToken = token test Nothing
   where
     test TokenPos {tok} = Right tok
 
 
-identifier :: Graciela Text
+identifier :: (MonadParsec e s m, Prim.Token s ~ TokenPos)
+           => m Text
 identifier = token test Nothing
   where
     test    TokenPos {tok = TokId i} = Right i
     test tp@TokenPos {tok}           = Left . unex $ tp
 
 
-boolLit :: Graciela Bool
+boolLit :: (MonadParsec e s m, Prim.Token s ~ TokenPos)
+        => m Bool
 boolLit = token test Nothing
   where
     test    TokenPos {tok = TokBool b} = Right b
     test tp@TokenPos {tok}             = Left . unex $ tp
 
 
-charLit :: Graciela Char
+charLit :: (MonadParsec e s m, Prim.Token s ~ TokenPos)
+        => m Char
 charLit = token test Nothing
   where
     test    TokenPos {tok = TokChar c} = Right c
     test tp@TokenPos {tok}             = Left . unex $ tp
 
 
-stringLit :: Graciela Text
+stringLit :: (MonadParsec e s m, Prim.Token s ~ TokenPos)
+          => m Text
 stringLit = token test Nothing
   where
     test    TokenPos {tok = TokString s} = Right s
     test tp@TokenPos {tok}               = Left . unex $ tp
 
 
-integerLit :: Graciela Integer
+integerLit :: (MonadParsec e s m, Prim.Token s ~ TokenPos)
+           => m Integer
 integerLit = token test Nothing
   where
     test    TokenPos {tok = TokInteger i} = Right i
     test tp@TokenPos {tok}                = Left . unex $ tp
 
 
-floatLit :: Graciela Double
+floatLit :: (MonadParsec e s m, Prim.Token s ~ TokenPos)
+         => m Double
 floatLit = token test Nothing
   where
     test    TokenPos {tok = TokFloat f} = Right f
